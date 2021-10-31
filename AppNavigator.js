8aefb80d533657fd2ea67ca1a5a65514136f63fb
Tuong Long <tuongtranhailong2804@gmail.com>
2021-11-01 00:37:30 +0700

    6

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { Text, View, Button, StyleSheet, Image } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Categories from "./screens/Categories";
 import Category from "./screens/Category";
 import userScreen from './screens/UserScreen';
 import cartScreen from './screens/Cartscreen';
+
 
 const AppNavigator = createNativeStackNavigator();
 
@@ -19,7 +21,97 @@ const App = ({ navigation }) => {
     </AppNavigator.Navigator>
   );
 }
-export default App;
+export { App };
+
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          position: 'absolute',
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: '#ffffff',
+          borderRadius: 15,
+          height: 90,
+          ...styles.shadow
+
+        }
+      }}
+    >
+      <Tab.Screen name='Home' component={Categories} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={{ uri: 'https://res.cloudinary.com/dsg1yo23i/image/upload/v1635699924/home_wiaobr.png' }}
+              resizeMode='contain'
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: focused ? `#000000` : `#a9a9a9`
+              }}
+
+            />
+          </View>
+        ),
+      }} />
+      <Tab.Screen name='Cart' component={cartScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={{ uri: 'https://res.cloudinary.com/dsg1yo23i/image/upload/v1635699925/shopping-cart_scuimp.png' }}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? `#000000` : `#a9a9a9`
+              }}
+
+            />
+          </View>
+        ),
+      }} />
+
+      <Tab.Screen name='User' component={userScreen} options={{
+        tabBarIcon: ({ focused }) => (
+          <View>
+            <Image
+              source={{ uri: 'https://res.cloudinary.com/dsg1yo23i/image/upload/v1635699924/user_1_clbcf1.png' }}
+              resizeMode='contain'
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? `#000000` : `#a9a9a9`
+              }}
+
+            />
+          </View>
+        ),
+      }} />
+    </Tab.Navigator>
+  );
+}
+
+export default Tabs;
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#ffffff',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5
+  }
+});
+
+
+
 
 
 
--- Components/tabs.js
+++ Components/tabs.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Text, View, Button, StyleSheet, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Categories from "../screens/Categories";
 import userScreen from '../screens/UserScreen';
@@ -10,13 +11,84 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name='Home' component={Categories} />
-            <Tab.Screen name='Cart' component={cartScreen} />
-            <Tab.Screen name='User' component={userScreen} />
+        <Tab.Navigator
+            tabBarOptions={{
+                showLabel: false,
+                style: {
+                    position: 'absolute',
+                    bottom: 25,
+                    left: 20,
+                    right: 20,
+                    elevation: 0,
+                    backgroundColor: '#ffffff',
+                    borderRadius: 15,
+                    height: 90,
+                    ...styles.shadow
+
+                }
+            }}
+        >
+            <Tab.Screen name='Home' component={Categories} options={{
+                tabBarIcon: ({ focused }) => (
+                    <View>
+                        <Image
+                            source={{ uri: 'https://res.cloudinary.com/dsg1yo23i/image/upload/v1635699924/home_wiaobr.png' }}
+                            resizeMode='contain'
+                            style={{
+                                width: 20,
+                                height: 20,
+                                tintColor: focused ? `#000000` : `#a9a9a9`
+                            }}
+
+                        />
+                    </View>
+                ),
+            }} />
+            <Tab.Screen name='Cart' component={cartScreen} options={{
+                tabBarIcon: ({ focused }) => (
+                    <View>
+                        <Image
+                            source={{ uri: 'https://res.cloudinary.com/dsg1yo23i/image/upload/v1635699925/shopping-cart_scuimp.png' }}
+                            resizeMode='contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? `#000000` : `#a9a9a9`
+                            }}
+
+                        />
+                    </View>
+                ),
+            }} />
+
+            <Tab.Screen name='User' component={userScreen} options={{
+                tabBarIcon: ({ focused }) => (
+                    <View>
+                        <Image
+                            source={{ uri: 'https://res.cloudinary.com/dsg1yo23i/image/upload/v1635699924/user_1_clbcf1.png' }}
+                            resizeMode='contain'
+                            style={{
+                                width: 25,
+                                height: 25,
+                                tintColor: focused ? `#000000` : `#a9a9a9`
+                            }}
+
+                        />
+                    </View>
+                ),
+            }} />
         </Tab.Navigator>
     );
 }
 
 export default Tabs;
 
+const styles = StyleSheet.create({
+    shadow: {
+        shadowColor: '#ffffff',
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5
+    }
+});
